abc41a10152d8f8d476c55f4e10348e8c2046ee1
circular2022 <circularconstant2015@gmail.com>
2023-06-29 00:49:59 +0900

    @Valueの実験

--- springTest/src/main/java/com/example/springTest/controller/SpringRestController.java
+++ springTest/src/main/java/com/example/springTest/controller/SpringRestController.java
@@ -1,6 +1,10 @@
 package com.example.springTest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +18,10 @@ import com.example.springTest.mapper.CommentsMapper;
 import com.example.springTest.mapper.CommentsMapperExtends;
 import com.example.springTest.model.Articles;
 import com.example.springTest.service.SpringTestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +31,9 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping(value = "/spring")
-public class SpringTestRestController {
+public class SpringRestController {
 
-    Logger logger = LoggerFactory.getLogger(SpringTestController.class);
+    Logger logger = LoggerFactory.getLogger(SpringRestController.class);
 
     @Autowired
     ArticlesMapper articlesMapper;
@@ -43,19 +51,23 @@ public class SpringTestRestController {
     SpringTestService springTestService;
 
     @Autowired
+    // @Qualifier("bean1")
     long numOfArticles;
+
+    @Value("${dirpath.list}")
+    String dirPath;
 
     @PostMapping(value = "/aaa")
     // public String searchArticles(@RequestBody @Validated Message message) {
     // application/jsonとしてパラメーターを受け取る場合は、DTO に@RequestBodyを付与
-    public long aaa(@RequestBody Message message) {
+    public long aaa(@Validated @RequestBody Message message) {
 
         System.out.println(numOfArticles);
 
         long startTime = System.currentTimeMillis();
         
         logger.info("access POST /aaa");
-
+        
         List<Articles> resultList = new ArrayList<Articles>();
 
         try{
@@ -74,7 +86,7 @@ public class SpringTestRestController {
 
     @PostMapping(value = "/bbb")
     // public String searchArticles(@RequestBody @Validated Message message) {
-    public String bbb() {
+    public ResponseEntity<Void> bbb() throws Exception {
 
         long startTime = System.currentTimeMillis();
         
@@ -85,14 +97,59 @@ public class SpringTestRestController {
         try{
             resultList = springTestService.getArticles("あ", true);
         }catch(Exception e){
-            return "error";
+            // return "error";
         }
 
         long endTime = System.currentTimeMillis();
         logger.info("bbb 件数：{}", resultList.size());
         logger.info("bbb 処理時間：" + (endTime - startTime) + "ms");
 
-        return "bbb";
+        return new ResponseEntity<>(HttpStatus.OK);
+        // return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        // return null;
+        // throw(new Exception());
 
     }
+
+    @PostMapping(value = "/sample")
+    // public ResponseEntity<Void> sample(@RequestBody Message message) {
+    public ResponseEntity<Void> sample(@RequestBody String message) throws Exception {        
+        System.out.println(message);
+
+        // ObjectMapper objectMapper = new ObjectMapper();
+
+        // String jsonAsString;
+        // try {
+        //     jsonAsString = objectMapper.writeValueAsString(message1);                    
+        //     System.out.println(jsonAsString);
+        // } catch (JsonProcessingException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+        throw new Exception();
+        // return null;
+
+        // return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/sample2")
+    // public ResponseEntity<Void> sample(@RequestBody Message message) {
+    public ResponseEntity<Void> sample2(@RequestBody String message) throws Exception {        
+        System.out.println(message);
+
+        // ObjectMapper objectMapper = new ObjectMapper();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/sample3")
+    // public ResponseEntity<Void> sample(@RequestBody Message message) {
+    public ResponseEntity<Void> sample3(@RequestBody String message) throws Exception {        
+        System.out.println(dirPath);
+
+        // ObjectMapper objectMapper = new ObjectMapper();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
