e7c7c728b7d973fa82b67518e129861665e23012
circular2022 <circularconstant2015@gmail.com>
2023-07-20 01:00:43 +0900

    AppConfigにBeanを追加。

--- springTest/src/main/java/com/example/springTest/AppConfig.java
+++ springTest/src/main/java/com/example/springTest/AppConfig.java
@@ -1,10 +1,14 @@
 package com.example.springTest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import com.example.springTest.mapper.ArticlesMapper;
+import com.example.springTest.model.Articles;
 import com.example.springTest.model.ArticlesExample;
 
 @Configuration
@@ -14,9 +18,46 @@ public class AppConfig {
     ArticlesMapper articlesMapper;
     
     @Bean
-    public long create(){
+    public long configMethod0(){
         System.out.println("AppConfigのcreate");
         long numOfArticles = articlesMapper.countByExample(new ArticlesExample());
         return numOfArticles;
     }
+
+    @Bean
+    public String configMethod00(Environment env){
+        System.out.println(env.getProperty("dirpath.list"));
+        return env.getProperty("dirpath.list");
+    }
+
+    @Bean(name="bean1")
+    public long configMethod1(){
+        System.out.println("configMethod1");
+        return 1L;
+    }
+
+    @Bean(name="bean2")
+    public long configMethod2(){
+        System.out.println("configMethod2");
+        return 2L;
+    }
+
+    @Bean(name="bean3")
+    public long configMethod3(){
+        System.out.println("configMethod3");
+        return 2L;
+    }
+
+    @Bean(name="bean4")
+    public long configMethod4(){
+        System.out.println("configMethod4");
+        return 2L;
+    }
+
+    @Bean(name="bean5")
+    public long configMethod5(){
+        System.out.println("configMethod5");
+        return 2L;
+    }
+
 }
--- springTest/src/main/java/com/example/springTest/SpringTestApplication.java
+++ springTest/src/main/java/com/example/springTest/SpringTestApplication.java
@@ -1,6 +1,5 @@
 package com.example.springTest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +9,7 @@ public class SpringTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 
-		System.out.println("aaaaaaa");
+		System.out.println("SpringTestApplication");
 		// System.out.println(fileDirConfig.);
 	}
-	
 }
