3dc82d1a17799eb41930a23619c625617b100212
circular2022 <circularconstant2015@gmail.com>
2023-07-26 00:05:17 +0900

    WebAPI追加

--- common/demo/src/main/java/com/peechan/common/CallWebApi.java
+++ common/demo/src/main/java/com/peechan/common/CallWebApi.java
@@ -11,9 +11,9 @@ public class CallWebApi {
 
     static RestTemplate restTemplate = new RestTemplate();
  
-    public void callWebApiSample(){
+    public void callWebApiAAA(){
 
-        System.out.println("callWebApiSample開始");
+        System.out.println("callWebApiAAA開始");
 
         try{
             String url = "http://127.0.0.1:10000/spring/aaa";
@@ -29,7 +29,30 @@ public class CallWebApi {
             System.out.println(result.getStatusCode());
             System.out.println(result.getBody());
             
-            System.out.println("callWebApiSample終わり");
+            System.out.println("callWebApiAAA終わり");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void callWebApiFFF(){
+        System.out.println("callWebApiFFF開始");
+
+        try{
+            String url = "http://127.0.0.1:10000/spring/fff";
+
+            RestMessage restMessage = new RestMessage(){{
+                setAaa("aaa");
+            }};
+
+            RequestEntity<RestMessage> req = 
+                RequestEntity.post(url).contentType(MediaType.APPLICATION_JSON).body(restMessage);
+
+            ResponseEntity<Long> result = restTemplate.postForEntity(url, req, Long.class);
+            System.out.println(result.getStatusCode());
+            System.out.println(result.getBody());
+            
+            System.out.println("callWebApiFFF終わり");
         }catch(Exception e){
             e.printStackTrace();
         }
